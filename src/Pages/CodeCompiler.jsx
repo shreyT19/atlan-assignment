012ba1383e12143a348d4ae0860c0a42d874399c
shreyT19 <shreyanshtripathy49@gmail.com>
2023-08-07 09:23:51 +0530

    perf: cached data using useMemo

--- src/Pages/CodeCompiler.jsx
+++ src/Pages/CodeCompiler.jsx
@@ -1,17 +1,13 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense, lazy,useMemo } from "react";
 //components
-// import Table from "../components/Table/Table";
 import Toolbar from "../components/ToolBar/Toolbar";
 //css
 import "./CodeCompiler.css";
 //data
 import dummyData from "../assets/data.json";
-// import customersData from "../assets/customers.json";
+import customersData from "../assets/customers.json";
 // import ordersData from "../assets/orders.json";
-
-// context api hook for state management
 import { useStateContext } from "../context/Context";
-// import EntitySchema from "../components/EntitySchema/EntitySchema";
 
 const EntitySchema = lazy(() =>
   import("../components/EntitySchema/EntitySchema")
@@ -29,21 +25,47 @@ const CodeCompiler = () => {
   } = useStateContext();
   const [error, setError] = React.useState(false);
 
+  const memoizedEntitySchemas = useMemo(() => {
+    console.log("memoizedEntitySchemas");
+    return <Suspense fallback={<div>Loading Entity Schema...</div>}>
+      <div className="entityModels">
+        <EntitySchema heading="Customers" schemaData={dummyData?.orders} />
+        <EntitySchema heading="Orders" schemaData={dummyData?.customers} />
+      </div>
+    </Suspense>
+}, [dummyData]);
+
+  const memoizedTableData = useMemo(() => {
+    
+    return <Suspense fallback={<div>Loading Table Data...</div>}>
+      <div className="questionDataTable">
+        <div className="flex2">
+          <p className="output">Customers</p>
+          <Table rows={dummyData?.customers} />
+        </div>
+        <div className="flex2">
+          <p className="output">Orders</p>
+          <Table rows={dummyData?.orders} />
+        </div>
+        {importedData?.length > 0 && (
+          <div className="flex2">
+            <p className="output">Imported Data</p>
+            <Table rows={importedData} />
+          </div>
+        )}
+      </div>
+    </Suspense>
+}, [dummyData, importedData]);
+
   return (
     <div className="compiler">
       <div className="logo">
         <p className="output head">Online SQL Editor</p>
       </div>
       <div className="flex">
-        <Suspense fallback={<div>Loading Entity Schema...</div>}>
-          <div className="entityModels">
-            <EntitySchema heading="Customers" schemaData={dummyData?.orders} />
-            <EntitySchema heading="Orders" schemaData={dummyData?.customers} />
-          </div>
-        </Suspense>
+        {memoizedEntitySchemas}
         <div className="code">
           <Toolbar />
-
           <textarea
             value={currentQuery}
             onChange={(e) => {
@@ -51,7 +73,6 @@ const CodeCompiler = () => {
               setCurrentQuery(e.target.value);
             }}
           ></textarea>
-
           <div className="outputSection">
             <p className="output">Output</p>
             {error && (
@@ -60,30 +81,9 @@ const CodeCompiler = () => {
               </p>
             )}
             {buttonClicked && <Table rows={currentOutputQuery} />}
-            {/* {buttonClicked && <Table rows={customersData} />} */}
           </div>
         </div>
-        <Suspense fallback={<div>Loading Table Data...</div>}>
-          <div className="questionDataTable">
-            <div className="flex2">
-              <p className="output">Customers</p>
-              <Table rows={dummyData?.customers} />
-              {/* <Table rows={customersData} /> */}
-            </div>
-            <div className="flex2">
-              <p className="output">Orders</p>
-              <Table rows={dummyData?.orders} />
-              {/* <Table rows={ordersData} /> */}
-            </div>
-
-            {importedData?.length > 0 && (
-              <div className="flex2">
-                <p className="output">Imported Data</p>
-                {importedData?.length > 0 && <Table rows={importedData} />}
-              </div>
-            )}
-          </div>
-        </Suspense>
+        {memoizedTableData}
       </div>
     </div>
   );
